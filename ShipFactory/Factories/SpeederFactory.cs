119f7ac879551b05c9fcf69400642d6d44c2e7de
AperanoGavin <audesandrine6@gmail.com>
2024-07-22 09:24:38 +0200

    update speeder

--- ShipFactory/Factories/SpeederFactory.cs
+++ ShipFactory/Factories/SpeederFactory.cs
@@ -5,7 +5,7 @@ namespace ShipFactory
         public Ship CreateShip()
         {
             var ship = new Ship("Speeder");
-            ship.Parts = new List<string> { "Hull_HS1", "Engine_ES1", "Wings_WS1", "Thruster_TS1", "Thruster_TS1" };
+            ship.Parts = new List<string> { "Hull_HS1", "Engine_ES1", "Wings_WS1", "Wings_WS1" , "Thruster_TS1", "Thruster_TS1" };
             return ship;
         }
     }
